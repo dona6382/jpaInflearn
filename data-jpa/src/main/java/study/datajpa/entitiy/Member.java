c51a4b43b802d5731c50a362593d8db0e90456e3
dona6382 <dona6382@naver.com>
2020-04-03 17:10:49 +0900

    middle commit

---
+++ data-jpa/src/main/java/study/datajpa/entitiy/Member.java
@@ -0,0 +1,15 @@
+package study.datajpa.entitiy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+
+}
