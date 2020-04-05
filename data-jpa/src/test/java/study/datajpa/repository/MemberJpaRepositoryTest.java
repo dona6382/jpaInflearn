22b106b51baf64270c7bb5b30a474bffd0da995e
dona6382 <dona6382@naver.com>
2020-04-05 22:13:57 +0900

    20200405 commit

--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import study.datajpa.entitiy.Member;
 
 import javax.transaction.Transactional;
@@ -12,7 +13,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-class MemberJpaRepositoryTest {
+@Rollback(false)
+public class MemberJpaRepositoryTest {
 
     @Autowired MemberJpaRepository memberJpaRepository;
 
