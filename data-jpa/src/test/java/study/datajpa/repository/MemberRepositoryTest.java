50144f84b9109fba629d96b318b26061f0a56582
dona6382 <dona6382@naver.com>
2020-04-07 18:27:36 +0900

    change package Name(entitiy -> entity) and add memberTest

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,13 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import study.datajpa.entitiy.Member;
+import study.datajpa.entity.Member;
 
 import javax.transaction.Transactional;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
