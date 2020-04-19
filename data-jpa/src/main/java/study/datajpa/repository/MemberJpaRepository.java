362ebecce7b4e6f82cec5acfab3eec3759d4e505
dona6382 <dona6382@naver.com>
2020-04-19 13:51:27 +0900

    add paging and totalCount

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -55,10 +55,17 @@ public class MemberJpaRepository {
     }
 
     public List<Member> findByPage(int age, int offset, int limit){
-        em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
                 .setParameter("age", age)
                 .setFirstResult(offset)
                 .setMaxResults(limit)
                 .getResultList();
     }
+
+    public long totalCount(int age){
+        return  em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
+
 }
